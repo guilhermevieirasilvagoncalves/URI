8ca90fad09aad31675fc4b1e404844729030177e
Guilherme Vieira <88863957+guilhermevieirasilvagoncalves@users.noreply.github.com>
2022-12-02 16:40:20 -0300

    Create bee1198.cpp

---
+++ bee1198.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    long long int a,b;
+    while (cin >> a >> b)
+    {
+        cout << abs(a-b) << endl;
+    }
+    
+}
