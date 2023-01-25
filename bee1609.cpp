08df6e705538ac9e29701d2e27d2fbb62049653b
Guilherme Vieira <88863957+guilhermevieirasilvagoncalves@users.noreply.github.com>
2023-01-25 19:15:53 -0300

    Create bee1609.cpp

---
+++ bee1609.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        set<int> s;
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+            s.insert(a);
+        }
+        cout << s.size() << endl;
+    }
+}
